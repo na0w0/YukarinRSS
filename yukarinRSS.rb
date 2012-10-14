d0fad5bbe21ea0ec63958185ec722d0efa7df9f1
strada3127 <sutoradaf2@gmail.com>
2012-10-15 01:54:12 +0900

    div#*.info-news-box div#*.info-news-box-hiddnを検索できなかったのでdiv id=info-right-box を検索して先に書いた項目を表示できるようにした。

--- yukarinRSS.rb
+++ yukarinRSS.rb
@@ -2,5 +2,5 @@ require "nokogiri"
 require "open-uri"
 
 yukarinRSS = Nokogiri::HTML(open("http://www.tamurayukari.com/information/index.html"))
-yukarinRSS.css('div.info-news-box', 'div.info-news-box-hidden').inner_text
-puts yukarinRSS
+puts yukarinRSS.css('//div[@id="info-right-box"]').text
+	

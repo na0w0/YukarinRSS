15b041f2ba815b951e351ae7b14692a1f9ec564d
stara3127 <sutoradaf2@gmail.com>
2012-10-17 14:27:20 +0900

    id info-entry-titleの順に表示できるようになった

--- test.rb
+++ test.rb
@@ -5,5 +5,9 @@ require "open-uri"
 rss = Nokogiri::HTML(open("http://www.tamurayukari.com/information/index.html"))
 
 rss.search('//div[@id="info-right-box"]/div').each do |div|
-	puts div['id'] 
+	puts div['id']
+	puts div.search('h3').text
 end
+#id
+#info-entry-title
+#の順にidごとに表示させたい

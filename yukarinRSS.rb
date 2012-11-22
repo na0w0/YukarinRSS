2d9e3c6ee369d02eda23530a1ec3cdcbb9d9aad3
stara3127 <sutoradaf2@gmail.com>
2012-11-22 12:05:48 +0900

    rssの出力成功

--- yukarinRSS.rb
+++ yukarinRSS.rb
@@ -1,5 +1,22 @@
+# coding: utf-8
 require "nokogiri"
 require "open-uri"
+require "rss"
 
-rss = Nokogiri::HTML(open("http://www.tamurayukari.com/information/index.html"))
-puts rss.css('//div[@id="info-right-box"]').text
+rss = RSS::Maker.make("2.0") do |maker|
+  maker.channel.about = "rss.rdf" #rssの名前
+  maker.channel.title = "information" #タイトル
+  maker.channel.description = "informationの一覧を表示" #説明文
+  maker.channel.link = "http://www.tamurayukari.com/information/index.html" #アクセスするサイト
+  maker.items.do_sort = true #並べ替えをするかしないか
+
+  doc = Nokogiri::HTML(open("http://www.tamurayukari.com/information/index.html")) #スクレイピングをする対象のサイト
+  doc.search('//div[@id="info-right-box"]/div').each do |div|
+    id = div['id']
+    title = div.search('h3').inner_text
+    item = maker.items.new_item #itemを生成
+    item.link = 'http://www.tamurayukari.com/information/index.html#' + id 
+    item.title = title
+  end
+end
+puts rss.to_s

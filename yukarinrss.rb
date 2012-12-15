75728263b18b1b2aaa0f30040e5c5e2b3e908b8b
strada3127 <sutoradaf2@gmail.com>
2012-12-15 13:04:51 +0900

    新しく作って登録

---
+++ yukarinrss.rb
@@ -0,0 +1,23 @@
+require "nokogiri"
+require "open-uri"
+require "rss"
+
+rss = RSS::Maker.make("2.0") do |maker|
+  maker.channel.about = "rss.rdf" #rssの名前
+  maker.channel.title = "information" #タイトル
+  maker.channel.description = "informationの一覧を表示" #説明文
+  maker.channel.link = "http://www.tamurayukari.com/information/index.html" #アクセスするサイト
+  maker.items.do_sort = true #並べ替えをするかしないか
+
+  doc = Nokogiri::HTML(open("http://www.tamurayukari.com/information/index.html")) #スクレイピングをする対象のサイト
+  doc.search('//div[@id="info-right-box"]/div').each do |div|
+    id = div['id']
+    title = div.search('h3').inner_text
+    info_entry_text = div.search('p').inner_text
+    item = maker.items.new_item #itemを生成
+    item.link = 'http://www.tamurayukari.com/information/index.html#' + id 
+    item.title = title
+    item.description = info_entry_text
+  end
+end
+puts rss.to_s
